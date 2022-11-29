d525d1e62dc2ae6bc5dc745a5cfda034146caa82
Duck <kondratev_28@inbox.ru>
2022-11-29 22:37:12 +0300

    Задача 8

--- Program.cs
+++ Program.cs
@@ -59,3 +59,17 @@ else
     Console.WriteLine("Нет :(");
 }
 */
+
+//Задача 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+/*
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int n = 0;
+
+while (n<num - 1)
+{
+    n = n + 2;
+    Console.Write(n + " ");
+}
+*/
